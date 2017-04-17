bd4f4eb3c68ce44d44efc9a1a6c8db42c5452bd7
Jamal Rorie <jrorie@rice.edu>
2017-04-17 17:56:41 -0500

    Expanding back to 1D UCI test. Also: added AUC readout to log

--- templates/macros.py
+++ templates/macros.py
@@ -20,7 +20,7 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.preprocessing import maxabs_scale
+from sklearn.preprocessing import maxabs_scale, robust_scale, minmax_scale
 from sklearn.model_selection import GridSearchCV
 
 import globals
@@ -39,66 +39,62 @@ def scale_x(X_train_prescale, X_test_prescale, scaler):
 		X_train = robust_scale(X_train_prescale)
 		X_test  = robust_scale(X_test_prescale)
 		print 'Robust Scale\n'
+	elif scaler == 'minmax':
+                X_train = minmax_scale(X_train_prescale)
+                X_test  = minmax_scale(X_test_prescale)				
 	return X_train, X_test
 
-#def create_model(input_scale):
 def create_model():
         input_model = Sequential()
         input_model.add(Dense(100, input_dim=globals.input_scale, init='uniform', activation='relu')) #Input layer
-        input_model.add(Dense(100, init='uniform', activation='relu'))               #Hidden Layer 02
-        input_model.add(Dense(100, init='uniform', activation='relu'))               #Hidden Layer 03
-#        model.add(Dense(input_scale, init='uniform', activation='relu'))               #Hidden Layer 04
-#        model.add(Dense(input_scale, init='uniform', activation='relu'))               #Hidden Layer 05
-#        model.add(Dense(input_scale, init='uniform', activation='relu'))               #Hidden Layer 06
-#        model.add(Dense(input_scale, init='uniform', activation='relu'))               #Hidden Layer 07
-#        model.add(Dense(input_scale, init='uniform', activation='relu'))               #Hidden Layer 08
-#        model.add(Dense(input_scale, init='uniform', activation='relu'))               #Hidden Layer 09
-#        model.add(Dense(input_scale, init='uniform', activation='relu'))               #Hidden Layer 10
-        input_model.add(Dense(1, init='uniform', activation='sigmoid'))
+        input_model.add(Dense(100, init='uniform', activation='relu'))		  		      #Hidden Layer 02
+        input_model.add(Dense(100, init='uniform', activation='relu'))               		      #Hidden Layer 03
+        input_model.add(Dense(1, init='uniform', activation='sigmoid'))              		      #Output Layer
         # Compile model
         input_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         return input_model
 
+def create_model_UCI_1D_default():
+        input_model = Sequential()
+        input_model.add(Dense(100, input_dim=globals.input_scale, init='uniform', activation='sigmoid')) #Input layer
+        input_model.add(Dense(100, init='uniform', activation='sigmoid'))                                #Hidden Layer 02
+        input_model.add(Dense(100, init='uniform', activation='sigmoid'))                                #Hidden Layer 03
+        input_model.add(Dense(1, init='uniform', activation='sigmoid'))                                  #Output Layer
+        # Compile model
+	sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+	input_model.compile(loss='mean_squared_error', optimizer=sgd, metrics=['accuracy'])
+        return input_model
 
-def create_model_neurons(input_scale = globals.input_scale, neurons = 40): 
+def create_model_neurons(neurons = 40): 
         input_model = Sequential()
         input_model.add(Dense(neurons, input_dim=globals.input_scale, init='uniform', activation='relu')) #Input layer
-        input_model.add(Dense(neurons, init='uniform', activation='relu'))               #Hidden Layer 02
-        input_model.add(Dense(neurons, init='uniform', activation='relu'))               #Hidden Layer 03
-        input_model.add(Dense(1, init='uniform', activation='sigmoid'))
+        input_model.add(Dense(neurons, init='uniform', activation='relu'))              		  #Hidden Layer 02
+        input_model.add(Dense(neurons, init='uniform', activation='relu'))               		  #Hidden Layer 03
+        input_model.add(Dense(1, init='uniform', activation='sigmoid'))					  #Output Layer
 	# Compile model
 	input_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         return input_model
 
-#def create_model_neurons(input_dim=4, neurons=40):
-#        input_model = Sequential()
-#        input_model.add(Dense(neurons, input_dim=input_scale, init='uniform', activation='relu')) #Input layer
-#        input_model.add(Dense(neurons, init='uniform', activation='relu'))               #Hidden Layer 02
-#        input_model.add(Dense(neurons, init='uniform', activation='relu'))               #Hidden Layer 03
-#        input_model.add(Dense(1, init='uniform', activation='sigmoid'))
-#        # Compile model
-#        input_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-#        return input_model
 
 
-def create_model_activation(input_scale=28, activation='tanh'):
+def create_model_activation(activation='tanh'):
         input_model = Sequential()
-        input_model.add(Dense(100, input_dim=input_scale, init='uniform', activation=activation)) #Input layer
-        input_model.add(Dense(100, init='uniform', activation=activation))               #Hidden Layer 02
-        input_model.add(Dense(100, init='uniform', activation=activation))               #Hidden Layer 03
-        input_model.add(Dense(1, init='uniform', activation='sigmoid'))
+        input_model.add(Dense(100, input_dim=globals.input_scale, init='uniform', activation=activation)) #Input layer
+        input_model.add(Dense(100, init='uniform', activation=activation))               		  #Hidden Layer 02
+        input_model.add(Dense(100, init='uniform', activation=activation))               		  #Hidden Layer 03
+        input_model.add(Dense(1, init='uniform', activation='sigmoid'))					  #Output Layer
         # Compile model
         input_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         return input_model
 
 
 
-def create_model_opt(input_scale=28, optimizer='adam'):
+def create_model_opt(optimizer='adam'):
         input_model = Sequential()
-        input_model.add(Dense(100, input_dim=input_scale, init='uniform', activation='relu')) #Input layer
-        input_model.add(Dense(100, init='uniform', activation='relu'))               #Hidden Layer 02
-        input_model.add(Dense(100, init='uniform', activation='relu'))               #Hidden Layer 03
-        input_model.add(Dense(1, init='uniform', activation='sigmoid'))
+        input_model.add(Dense(100, input_dim=globals.input_scale, init='uniform', activation='relu')) #Input layer
+        input_model.add(Dense(100, init='uniform', activation='relu'))               		      #Hidden Layer 02
+        input_model.add(Dense(100, init='uniform', activation='relu'))               		      #Hidden Layer 03
+        input_model.add(Dense(1, init='uniform', activation='sigmoid'))				      #Output Layer
         # Compile model
         input_model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
         return input_model
@@ -150,6 +146,30 @@ def plot_predictions(feature_arg, x_test_data, predictions, x, show_toggle, save
                 plt.show()
         plt.close()
 
+def plot_predictions_1D(feature_arg, x_test_data, predictions, x, show_toggle, save_toggle):
+        # Setup plot
+        bins_array = numpy.arange(-5,5,0.1)
+        plt.xlabel('Feature %d' % feature_arg)
+        plt.ylabel('Events')
+        plt.title('Distro of ttWW Reconstructed Mass')
+
+	x_test_data.ndim 
+ 	x_test_data.shape
+
+        # Plot predictions
+        plt.hist(x_test_data[numpy.nonzero(predictions.T==0)[1]][:,0], bins_array, alpha=0.3, color='r', label=['bkg'])
+        plt.hist(x_test_data[(numpy.nonzero((predictions.T) & (x_test_data[:,1]==1500))[1])][:,0], bins_array, alpha=0.3, color='y', label=['1500'])
+        plt.hist(x_test_data[(numpy.nonzero((predictions.T) & (x_test_data[:,1]==1250))[1])][:,0], bins_array, alpha=0.3, color='c', label=['1250'])
+        plt.hist(x_test_data[(numpy.nonzero((predictions.T) & (x_test_data[:,1]==1000))[1])][:,0], bins_array, alpha=0.3, color='g', label=['1000'])
+        plt.hist(x_test_data[(numpy.nonzero((predictions.T) & (x_test_data[:,1]==750))[1])][:,0], bins_array, alpha=0.3, color='b', label=['750'])
+        plt.hist(x_test_data[(numpy.nonzero((predictions.T) & (x_test_data[:,1]<500))[1])][:,0], bins_array, alpha=0.3, color='m', label=['500'])
+        plt.legend()
+        if save_toggle == True:
+                plt.savefig('./plots/27D_not1000_predictions_truemass_%02d.png' % x)
+        if show_toggle == True:
+                plt.show()
+        plt.close()
+
 def plot_ROC(y_test_data, predictions, x, show_toggle, save_toggle):
         #Plot ROC curve
         fpr, tpr, _ = roc_curve(y_test_data, predictions)
@@ -171,4 +191,4 @@ def plot_ROC(y_test_data, predictions, x, show_toggle, save_toggle):
         numpy.save('./roc_info/fpr_not1000_%02d' %x, fpr)
         numpy.save('./roc_info/tpr_not1000_%02d' %x, tpr)
         plt.close()
-
+	return roc_auc 
