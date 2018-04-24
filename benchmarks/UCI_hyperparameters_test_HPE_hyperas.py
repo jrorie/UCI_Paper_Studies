6f4b5f9b731af3cc047135f2519d2cf78dbbbc0c
Jamal Rorie <jrorie@rice.edu>
2018-04-24 16:33:44 -0500

    Added in CNN to see if there are any obvious python issues. Should not actually optimize.

--- benchmarks/UCI_hyperparameters_test_HPE_hyperas.py
+++ benchmarks/UCI_hyperparameters_test_HPE_hyperas.py
@@ -161,6 +161,45 @@ def create_model(x_train, y_train, x_test, y_test):
     return {'loss': -acc, 'status': STATUS_OK, 'model': model}
 
 
+
+def create_model_CNN(x_train, y_train, x_test, y_test):
+    """
+    Model providing function:
+
+    Create Keras model with double curly brackets dropped-in as needed.
+    Return value has to be a valid python dictionary with two customary keys:
+        - loss: Specify a numeric evaluation metric to be minimized
+        - status: Just use STATUS_OK and see hyperopt documentation if not feasible
+    The last one is optional, though recommended, namely:
+        - model: specify the model just created so that we can later use it again.
+    """
+    input_model = Sequential()
+    input_model.add(Input(shape=(height, width, depth)))                                                         #Input
+    input_model.add(Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu'))    #Conv 1
+    input_model.add(Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu'))    #Conv 2
+    input_model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))                                                #Pool 1
+    input_model.add(Dropout(drop_prob_1))                                                                        #Drop 1
+    input_model.add(Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu'))    #Conv 3
+    input_model.add(Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu'))    #Conv 4
+    input_model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))                                                #Pool 2
+    input_model.add(Dropout(drop_prob_1))                                                                        #Drop 2
+    input_model.add(flat = Flatten())                                                                            #Flat
+    input_model.add(Dense(hidden_size, activation='relu'))                                                        #Hidden
+    input_model.add(Dropout(drop_prob_2))                                                                        #Drop 3
+    input_model.add(Dense(num_classes, activation='softmax'))                                                    #Output
+
+    input_model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
+              optimizer='adam', # using the Adam optimiser
+              metrics=['accuracy', 'mse']) # reporting the accuracy
+
+    input_model.fit(X_train, Y_train,                # Train the model using the training set...
+          batch_size=batch_size, epochs=num_epochs,
+          verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
+    print('Test accuracy:', acc)
+    return {'loss': -acc, 'status': STATUS_OK, 'model': input_model}
+
+
+
 if __name__ == '__main__':
     best_run, best_model = optim.minimize(model=create_model,
                                           data=data,
