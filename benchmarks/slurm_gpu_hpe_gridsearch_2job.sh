75a125d47a2a637ce295e1d5428d641da0f46302
Jamal Rorie <jrorie@rice.edu>
2018-06-07 11:25:14 -0500

    update before exploring reorg

--- benchmarks/slurm_gpu_hpe_gridsearch_2job.sh
+++ benchmarks/slurm_gpu_hpe_gridsearch_2job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source activate tf1.4.1_keras2.1.3_GPU
-python /home/rice/jrorie/UCI_Paper_Studies/benchmarks/UCI_hyperparameters_test_HPE_2job.py
+python /home/rice/jrorie/UCI_Paper_Studies/benchmarks/UCI_hyperparameters_test_HPE_gridsearch_2job.py
 
