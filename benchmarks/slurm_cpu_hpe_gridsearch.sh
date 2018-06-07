b18d86955f8616d8fda4f6c3cab0a10b7dbdc7bb
Jamal Rorie <jrorie@rice.edu>
2018-06-07 10:22:07 -0500

    Added more submission scripts

---
+++ benchmarks/slurm_cpu_hpe_gridsearch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source activate tf1.4.1_keras2.1.3_CPU
+python /home/rice/jrorie/UCI_Paper_Studies/templates/UCI_hyperparameters_test_HPE.py
---
+++ benchmarks/slurm_cpu_hpe_hyperas.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source activate test
+python /home/rice/jrorie/UCI_Paper_Studies/benchmarks/UCI_hyperparameters_test_HPE_hyperas.py
